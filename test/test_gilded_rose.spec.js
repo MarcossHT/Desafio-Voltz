0b5acfaf1c95f6fd3ff344523c933c7aad4bea8c
MarcossHT <mppiress@gmail.com>
2022-01-17 18:19:41 -0300

    Alterações no teste

--- test/test_gilded_rose.spec.js
+++ test/test_gilded_rose.spec.js
@@ -6,20 +6,20 @@ describe("Regra dos conjurados", function() {
 
   it("Quando o valor for menor do que 1 qualidade decrementada em 4", function() {
     const gildedRose = new Shop([ new Item("conjured mana cake", 0, 8) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(4);
-    expect(items[0].sellIn).to.equal(-1);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(4);
+    expect(gildedRose.items[0].sellIn).to.equal(-1);
   });
 
   it("Quando o valor for maior do que 0 qualidade decrementada em 2", function() {
     const gildedRose = new Shop([ new Item("conjured mana cake", 1, 8) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(6);
-    expect(items[0].sellIn).to.equal(0);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(6);
+    expect(gildedRose.items[0].sellIn).to.equal(0);
   });
 
 
@@ -30,11 +30,11 @@ describe("Regra do Aged Brie", function() {
 
   it("A qualidade do Aged Brie aumenta em 1 com o passar dos dias", function() {
     const gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(1);
-    expect(items[0].sellIn).to.equal(1);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(1);
+    expect(gildedRose.items[0].sellIn).to.equal(1);
   });
 
 });
@@ -43,20 +43,20 @@ describe("Regra dos itens genéricos", function() {
 
   it("A qualidade dos itens genéricos é decrementada em 1 com o passar dos dias", function() {
     const gildedRose = new Shop([ new Item("+5 Dexterity Vest", 1, 6) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(5);
-    expect(items[0].sellIn).to.equal(0);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(5);
+    expect(gildedRose.items[0].sellIn).to.equal(0);
   });
 
   it("A qualidade dos itens genéricos a partir de sellIn igual a zero é decrementada em 2", function() {
     const gildedRose = new Shop([ new Item("Elixir of the Mongoose", 0, 7) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(5);
-    expect(items[0].sellIn).to.equal(-1);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(5);
+    expect(gildedRose.items[0].sellIn).to.equal(-1);
   });
 });
 
@@ -65,11 +65,11 @@ describe("Regra do Sulfuras, item lendário", function() {
 
   it("A qualidade deste item é 80 e não se altera com o passar dos dias", function() {
     const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(80);
-    expect(items[0].sellIn).to.equal(-1);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(80);
+    expect(gildedRose.items[0].sellIn).to.equal(-1);
   });
 
 });
@@ -78,38 +78,38 @@ describe("Backstage passes to a TAFKAL80ETC concert", function() {
 
   it("A qualidade deste item aumenta com o passar dos dias: sellin menor ou igual a 5, qualidade sobe três pontos", function() {
     const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(23);
-    expect(items[0].sellIn).to.equal(4);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(23);
+    expect(gildedRose.items[0].sellIn).to.equal(4);
   });
 
   it("A qualidade deste item aumenta com o passar dos dias: sellin menor ou igual a 10, qualidade sobe dois pontos", function() {
     const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(22);
-    expect(items[0].sellIn).to.equal(9);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(22);
+    expect(gildedRose.items[0].sellIn).to.equal(9);
   });
 
   it("A qualidade deste item aumenta com o passar dos dias: sellin menor que 1, a qualidade irá zerar", function() {
     const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(0);
-    expect(items[0].sellIn).to.equal(-1);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(0);
+    expect(gildedRose.items[0].sellIn).to.equal(-1);
   });
 
   it("A qualidade deste item aumenta com o passar dos dias: sellin maior que 10, qualidade sobe um ponto ", function() {
     const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20) ]); 
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.be.a('number');
-    expect(items[0].sellIn).to.be.a('number');
-    expect(items[0].quality).to.equal(21);
-    expect(items[0].sellIn).to.equal(10);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).to.be.a('number');
+    expect(gildedRose.items[0].sellIn).to.be.a('number');
+    expect(gildedRose.items[0].quality).to.equal(21);
+    expect(gildedRose.items[0].sellIn).to.equal(10);
   });
 
 });
